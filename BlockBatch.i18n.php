21bbb16a4ac27f56a57d2a1278fd6c80afa70413
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2014-06-20 10:31:24 +0200

    Update BlockBatch.i18n.php
    Adding translations for German

--- BlockBatch.i18n.php
+++ BlockBatch.i18n.php
@@ -18,3 +18,20 @@ This should be done only to prevent vandalism, and in accordance with [[{{MediaW
 Fill in a specific reason below (for example, citing particular pages that were vandalized).',
         'blockbatch-ipbsubmit' => 'Block these users'
 );
+
+/** German (Deutsch)
+* @author Kghbln
+*/
+$messages[ 'de' ] = array(
+	'blockbatch' => "BlockBatch",
+	'blockbatch-desc' => "Ergänzt eine [[Special:BlockBatch|Spezialseite]] zum gesammelten Sperren von Benutzern",
+        'blockbatch-iptext' => 'Nutze das untenstehende Forumlar, um den Schreibzugriff bestimmter Benutzer oder IP-Adressen zu sperren. Dies sollte ausschließlich zur Verhinderung von Vandalismus und in Übereinstimmung mit der [[{{MediaWiki:Policy-url}}|Richtlinie diese Wikis]] erfolgen. Gib zudem einen Grund für die Sperrung an, bspw. den Hinweis auf die vadalisierten Inhalte.',
+        'blockbatch-ipbsubmit' => 'Benutzer sperren'
+);
+
+/** German (formal address) (Deutsch (Sie-Form)‎)
+ * @author Kghbln
+ */
+$messages[ 'de-formal' ] = array(
+        'blockbatch-iptext' => 'Nutzen Sie das untenstehende Forumlar, um den Schreibzugriff bestimmter Benutzer oder IP-Adressen zu sperren. Dies sollte ausschließlich zur Verhinderung von Vandalismus und in Übereinstimmung mit der [[{{MediaWiki:Policy-url}}|Richtlinie diese Wikis]] erfolgen. Geben Sie zudem einen Grund für die Sperrung an, bspw. den Hinweis auf die vadalisierten Inhalte.'
+        );
